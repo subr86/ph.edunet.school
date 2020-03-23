fae75b9cfa751c854162074c24df2e185a855001
Sergey Petrik <subr86@gmail.com>
2020-03-23 12:24:53 +0200

    add edunet theme

--- theme/go4moreschool/layout/g4mheader.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Developer2
- * Date: 24.09.2018
- * Time: 16:10
- */
--- theme/go4moreschool/lib.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// This line protects the file from being accessed by a URL directly.
-defined('MOODLE_INTERNAL') || die();
-?>
